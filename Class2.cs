fd3d8a97775a34e49ed4be0e0c0894bc9ad5061e
TranCongQuan <35591936+TranCongQuan@users.noreply.github.com>
2018-09-14 14:23:55 +0700

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Modules
+{
+    internal static class Class2
+    {
+        private static Timer loopingTimer;
+
+        internal static Task StartTimer()
+        {
+            loopingTimer = new Timer()
+            {
+                Interval = 5000,
+                AutoReset = true,
+                Enabled = true
+            };
+            loopingTimer.Elapsed += OnTimerTicked;
+            return Task.Run(() =>
+            {
+                // i'm just here to simulate hard work
+            });
+        }
+
+        private static async void OnTimerTicked(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
